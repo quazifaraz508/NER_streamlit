ecbe413b30bf2b9198b7e834f999e395a1177d13
quazifaraz508 <141524293+quazifaraz508@users.noreply.github.com>
2024-12-15 01:01:05 +0530

    Update new_app_ner_streamlit.py

--- new_app_ner_streamlit.py
+++ new_app_ner_streamlit.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="NER_prototype", layout="wide")
 st.title('This is named_entity_recognition app (NER).')
 st.header("from text it find OFFENSIVE WORDS, STATE ,CITIES and VILLAGE but for now it has limited data so may be some of data it can't find but most of the it will.")
 
-user_inp = st.text_input('Enter text/message: ','This is maharashtra not goa or bihar')
+user_inp = st.text_input('Enter text/message: ','This is maharashtra not goa or bihar, what the f**k, is he chutiya?')
 result = ner_logic_op.build(user_inp)
 st.write(result)
 
