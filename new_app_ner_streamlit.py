5b49f336800e75d072315dd59dd3a8bb54dfd0ff
quazifaraz508 <141524293+quazifaraz508@users.noreply.github.com>
2024-12-14 23:59:03 +0530

    Update new_app_ner_streamlit.py

--- new_app_ner_streamlit.py
+++ new_app_ner_streamlit.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from NER_logic import ner_logic
+from  offensive_words import offencive_word_detect
 ner_logic_op = ner_logic()
 
 st.set_page_config(page_title="NER_prototype", layout="wide")
@@ -9,3 +10,6 @@ st.header("from text it find STATE ,CITIES and VILLAGE but for now it has limite
 user_inp = st.text_input('Enter text/message: ','This is maharashtra not goa or bihar')
 result = ner_logic_op.build(user_inp)
 st.write(result)
+result_offens = offencive_word_detect(user_inp)
+st.write(result_offens)
+
