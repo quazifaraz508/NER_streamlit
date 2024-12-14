ac20daa3b8f6f66fa05b4b54258d0ebe602c5df8
quazifaraz508 <141524293+quazifaraz508@users.noreply.github.com>
2024-12-15 00:20:06 +0530

    Update offensive_words.py

--- offensive_words.py
+++ offensive_words.py
@@ -272,4 +272,4 @@ def offencive_word_detect(text):
     else:
         print("No offensive words detected.")
         
-    return return "\n".join(results) if result_list else "No matches found."
+    return return "\n".join(result_list) if result_list else "No matches found."
